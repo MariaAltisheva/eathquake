c3eb5c958ad9923341a0f1b8ead3a6e1b8f6488a
MariaAltysheva <altysheva_maria@mail.ru>
2022-07-07 21:29:30 +0300

    Initial commit

---
+++ eq_explore_data.py
@@ -0,0 +1,37 @@
+
+from plotly.graph_objs import Layout
+from plotly import offline
+from utils import get_questions_data_by_requests
+
+# Получаем данные по ссылке в виде json
+all_eq_data = get_questions_data_by_requests()
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats, hover_texts = [], [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    hover_texts.append(title)
+
+
+# Нанесение данных на карту
+data = [{'type': 'scattergeo',
+         'lon': lons,
+         'lat': lats,
+         'text': hover_texts,
+         'marker': {
+             'size': [5*mag for mag in mags],
+             'color': mags,
+             'colorscale': 'Viridis',
+             'reversescale': True,
+             'colorbar': {'title': 'Magnitude'}}}]
+
+my_layout = Layout(title='Global Eathquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_eathquakes.html')
---
+++ utils.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+
+def get_data_from_json_file():
+    """Получаем настройки из json файла new,
+    где храним ссылку на json с данными"""
+    with open('data/new.json') as file:
+        return json.load(file)
+
+
+def get_questions_data_by_requests():
+    """Запрашиваем из интернета,
+    используя requests, json с данными"""
+    request_url = get_data_from_json_file()['eq']
+    response = requests.get(request_url)
+    return response.json()
+
